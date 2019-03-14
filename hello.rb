ff3b280f4dcf9415626ba673b746d2d8442b7f87
puntodamar <punto.damar2@gmail.com>
2019-03-14 15:54:46 +0700

    hello world

---
+++ hello.rb
@@ -0,0 +1,19 @@
+puts "Hello World"
+print "Hello World\n"
+p "Hello World"
+
+
+
+# def say_hello
+#   puts "Hello World"
+# end
+#
+# p say_hello
+
+
+def say_hello(things_to_say)
+  puts things_to_say
+end
+
+say_hello "I love you rinrin"
+
