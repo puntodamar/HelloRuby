4a96992c237f17a14f8eba4e1f570b341c73cf38
puntodamar <punto.damar2@gmail.com>
2019-03-14 20:23:12 +0700

    string

---
+++ strings1.rb
@@ -0,0 +1,9 @@
+first_name  = "Mashur"
+last_name   = "Hossain"
+
+puts first_name + " " + last_name
+
+puts "My first name is #{first_name} and my last name is #{last_name}"
+
+puts "He asked, 'How are you today?'"
+puts  'He asked, \'How are you today?\''
