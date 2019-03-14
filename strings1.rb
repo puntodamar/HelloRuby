a8b37afce44a3c1a5b1b8c4580a43900adb4183b
puntodamar <punto.damar2@gmail.com>
2019-03-14 20:30:36 +0700

    user input

--- strings1.rb
+++ strings1.rb
@@ -6,4 +6,4 @@ puts first_name + " " + last_name
 puts "My first name is #{first_name} and my last name is #{last_name}"
 
 puts "He asked, 'How are you today?'"
-puts  'He asked, \'How are you today?\''
+puts 'He asked, \'How are you today?\''
---
+++ terminal_input.rb
@@ -0,0 +1,7 @@
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Thank you #{first_name} #{first_name}. Your full name reversed is #{(first_name+" "+last_name).reverse!}"
+
